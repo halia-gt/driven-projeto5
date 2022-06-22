e89e185ce556fdd5939c77812e96e463bd82e200
Nathália <nathalia.gramst@gmail.com>
2022-06-22 14:03:46 -0300

    Pegando array de mensagens do servidor via axios.get()

--- scripts.js
+++ scripts.js
@@ -1,48 +1,12 @@
-let serverMessages = [
-    {
-        from: "João",
-        to: "Todos",
-        text: "entra na sala...",
-        type: "status",
-        time: "09:21:45"
-	},
-	{
-		from: "João",
-		to: "Todos",
-		text: "Bom dia",
-		type: "message",
-		time: "09:22:28"
-	},
-    {
-		from: "Maria",
-		to: "João",
-		text: "Oi João :)",
-		type: "message",
-		time: "09:22:38"
-	},
-	{
-		from: "João",
-		to: "Maria",
-		text: "Oi gatinha quer tc?",
-		type: "private_message",
-		time: "09:22:48"
-	},
-    {
-        from: "Maria",
-        to: "Todos",
-        text: "sai da sala...",
-        type: "status",
-        time: "09:22:58"
-	},
-]
+let serverMessages = [];
 
-// const promise = axios.get('http://mock-api.driven.com.br/api/v6/uol/messages');
-// promise.then(getMessages);
+const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+promise.then(getMessages);
 
-// function getMessages(answer) {
-//     serverMessages = answer.data;
-//     displayMessages();
-// }
+function getMessages(answer) {
+    serverMessages = answer.data;
+    displayMessages();
+}
 
 function displayMessages () {
     const main = document.querySelector('main');
@@ -74,12 +38,11 @@ function displayMessages () {
                         <span><strong class="name">${message.from} </strong>reservadamente para <strong class="to">${message.to}: </strong>${message.text}</span>
                     </p>
                 `
+                break;
         }
         main.innerHTML += messageTemplate;
     }
 
     lastMessage = document.querySelector('.message-container:last-child');
     lastMessage.scrollIntoView();
-}
-
-displayMessages();
+}f
